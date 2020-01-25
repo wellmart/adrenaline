7ff2f96e817764cd5fbe9145c57032c30f27866b
Wellington Marthas <well@dyntime.com>
2020-01-25 13:47:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Adrenaline
+
+[![Build Status](https://travis-ci.org/wellmart/adrenaline.svg?branch=master)](https://travis-ci.org/wellmart/adrenaline)
+[![Swift 5](https://img.shields.io/badge/swift-5-blue.svg)](https://developer.apple.com/swift/)
+![Version](https://img.shields.io/badge/version-0.1.0-blue)
+[![Software License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+[![Swift Package Manager Compatible](https://img.shields.io/badge/swift%20package%20manager-compatible-blue.svg)](https://github.com/apple/swift-package-manager)
+
+A framework with lot of extensions to improve the development, more adrenaline on life.
+
+## Requirements
+
+Swift 5 and beyond.
+
+## Usage
+
+```swift
+import UIKit
+import Adrenaline
+
+func main() {
+    let items = [String](reserveCapacity: 100)
+    
+    if let item = items[safe: 50] {
+        NSLog(item)
+    }
+    
+    NSLog(666.string(paddingLeft: 7))
+    
+    let view = UIView().also {
+        $0.backgroundColor = .red
+    }
+}
+```
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ Sources/Profiler.swift
@@ -0,0 +1,99 @@
+//
+//  Adrenaline
+//
+//  Copyright (c) 2020 Wellington Marthas
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+import os
+
+public struct Profiler {
+    public struct Tracing {
+        @usableFromInline
+        let log: OSLog
+        
+        @usableFromInline
+        let name: StaticString
+        
+        @usableFromInline
+        let id: OSSignpostID
+        
+        @inlinable
+        init(log: OSLog, name: StaticString) {
+            self.log = log
+            self.name = name
+            self.id = OSSignpostID(log: log)
+            
+            os_signpost(.begin, log: log, name: name, signpostID: id)
+        }
+        
+        @inlinable
+        init(log: OSLog, name: StaticString, format: StaticString, arguments: CVarArg...) {
+            self.log = log
+            self.name = name
+            self.id = OSSignpostID(log: log)
+            
+            os_signpost(.begin, log: log, name: name, signpostID: id, format, arguments)
+        }
+        
+        @inlinable
+        public func event(_ name: StaticString) {
+            os_signpost(.event, log: log, name: name, signpostID: id)
+        }
+        
+        @inlinable
+        public func end() {
+            os_signpost(.end, log: log, name: name, signpostID: id)
+        }
+        
+        @inlinable
+        public func end(_ format: StaticString, _ arguments: CVarArg...) {
+            os_signpost(.end, log: log, name: name, signpostID: id, format, arguments)
+        }
+    }
+    
+    @usableFromInline
+    let log: OSLog
+    
+    @inlinable
+    public init(category: String) {
+        guard let subsystem = Bundle.main.bundleIdentifier else {
+            preconditionFailure("Must be valid bundle identifier")
+        }
+        
+        log = OSLog(subsystem: subsystem, category: category)
+    }
+    
+    @inlinable
+    public func begin(_ name: StaticString) -> Tracing {
+        return Tracing(log: log, name: name)
+    }
+    
+    @inlinable
+    public func begin(_ name: StaticString, _ format: StaticString, _ arguments: CVarArg...) -> Tracing {
+        return Tracing(log: log, name: name, format: format, arguments: arguments)
+    }
+    
+    @inlinable
+    public func event(_ name: StaticString) {
+        os_signpost(.event, log: log, name: name)
+    }
+}
